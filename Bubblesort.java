f73f87c2ef5adb21cb81c25e92f6b2599a938413
VedanshPundir <vedanshpundir43@gmail.com>
2024-09-29 23:38:18 +0530

    Bubble sort added

---
+++ Bubblesort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Bubblesort
+ {
+    //O(n^2) time complexity
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements in array:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+    
+}
